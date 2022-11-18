823ebc82947bc71d16d5ed33dae9d77ec0a9401f
WOO-BIN LEE <iopplm89@gmail.com>
2022-11-18 15:31:11 +0900

    Hello Project 추가함

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,23 @@
+// 이 클래스가 속해있는 패키지명을 밝혀주어야 한다.
+package test.main;
+
+/* <- 여러줄 주석 쓰는법
+ *  하나
+ *  두울
+ *  세엣
+ */
+
+// <- 한줄 주석 쓰는법
+// 공개된=(모든 패키지에서 사용할 수있는)
+//공개된 클래스, 클래스명 MainClass2
+
+public class MainClass2 {// 클래스의 시작
+	/*
+	 *  해당 메소드 명 : main
+	 *    ex) javascript에서 function main(){} 함수명 main 처럼 쓰임
+	 *  run 했을 때 실행이 시작되는 아주 특별한 메소드
+	 */
+	public static void main(String[] args) {
+		
+	}
+}// 클래스의 끝
